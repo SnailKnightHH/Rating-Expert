2c762e1d092ac217721770277f1b895d23cc7594
David Hu <y467hu@uwaterloo.ca>
2021-12-11 22:14:04 -0500

    comments almost done, have some bugs

---
+++ rating-expert/src/Features/comment.js
@@ -0,0 +1,44 @@
+import { Box, Grid, Typography, Paper } from "@mui/material";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+
+export default function Comment({ Email, Reason, Rating, Date }) {
+  return (
+    <Paper
+      sx={{
+        p: 2,
+        margin: "auto",
+        maxWidth: "70%",
+        flexGrow: 1,
+        marginTop: "2rem",
+      }}
+    >
+      <Grid container spacing={2}>
+        <Grid item>
+          <AccountCircle />
+        </Grid>
+        <Grid item xs={12} sm container>
+          <Grid item xs container direction="column" spacing={2}>
+            <Grid item xs>
+              <Typography gutterBottom variant="subtitle1" component="div">
+                {Email}
+              </Typography>
+              <Typography variant="body2" gutterBottom>
+                {Reason}
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography sx={{ cursor: "pointer" }} variant="body2">
+                Rating: {Rating}
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Typography variant="subtitle1" component="div">
+              {Date.split("T")[0]}
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+}
--- rating-expert/src/Pages/instanceDetail.js
+++ rating-expert/src/Pages/instanceDetail.js
@@ -5,14 +5,22 @@ import {
   Box,
   LinearProgress,
   Paper,
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
 } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 
 import { useParams } from "react-router";
 import { useHistory } from "react-router";
-import { useEffect, useState } from "react";
+import { useEffect, useState, Fragment } from "react";
 import axios from "axios";
 import { baseURL } from "../Constants/constants";
+import Comment from "../Features/comment";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import { useDispatch, useSelector } from "react-redux";
 
 const useStyles = makeStyles({
   title: {
@@ -49,6 +57,13 @@ const useStyles = makeStyles({
 
 export default function InstanceDetail() {
   const classes = useStyles();
+  const user = useSelector((state) => state.user.user);
+
+  const [comment, setComment] = useState("");
+
+  const changeComment = (event) => {
+    setComment(event.target.value);
+  };
 
   const customPaper = (rating) => {
     return (
@@ -70,6 +85,30 @@ export default function InstanceDetail() {
       >
         <Typography className={classes.rating}>{rating}</Typography>
       </Paper>
+    );
+  };
+
+  const roundTo2DecimalPlaces = (num) => {
+    return Math.round(num * 100) / 100;
+  };
+
+  const calculateRating = () => {
+    console.log(
+      instanceInfo.comments.reduce((prev, cur) => {
+        console.log(prev.rating, cur.rating);
+        return prev.rating + cur.rating;
+      }, 0),
+      instanceInfo.comments.length + 1
+    );
+    return roundTo2DecimalPlaces(
+      (instanceInfo.rating +
+        (instanceInfo.comments.length !== 0
+          ? instanceInfo.comments.reduce(
+              (prev, cur) => prev.rating + cur.rating,
+              0
+            )
+          : 0)) /
+        (instanceInfo.comments.length + 1)
     );
   };
 
@@ -95,6 +134,18 @@ export default function InstanceDetail() {
     history.goBack();
   };
 
+  const addComment = async (comment, email, name, rating) => {
+    await axios.post(`${baseURL}/main/:category/createComment`, {
+      params: { comment, email, name, rating },
+    });
+  };
+
+  const [commentRating, setCommentRating] = useState("");
+
+  const handleChange = (event) => {
+    setCommentRating(event.target.value);
+  };
+
   if (isLoading) {
     return (
       <Box sx={{ width: "100%" }}>
@@ -103,34 +154,132 @@ export default function InstanceDetail() {
     );
   } else if (!isLoading && instanceInfo != null) {
     return (
-      <Grid
-        container
-        direction="column"
-        alignItems="center"
-        justifyContent="center"
-      >
-        <Typography className={classes.title}>{instanceInfo.name}</Typography>
-        <Typography className={classes.sub_category}>
-          {instanceInfo.sub_category}
-        </Typography>
-        <Typography>{instanceInfo.date.split("T")[0]}</Typography>
-        {customPaper(instanceInfo.rating)}
-        <Box display="flex" justifyContent="center" marginTop="2.5rem">
-          <Typography>{"Description: "}</Typography>
-        </Box>
-        <Typography className={classes.descriptionAndReasons}>
-          {instanceInfo.description}
-        </Typography>
-        <Box display="flex" justifyContent="center" marginTop="2.5rem">
-          <Typography>{"Reason: "}</Typography>
-        </Box>
-        <Typography className={classes.descriptionAndReasons}>
-          {instanceInfo.reason}
-        </Typography>
-        <Button variant="contained" className={classes.back} onClick={Back}>
-          Back
-        </Button>
-      </Grid>
+      <Fragment>
+        <Grid
+          container
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Typography className={classes.title}>{instanceInfo.name}</Typography>
+          <Typography className={classes.sub_category}>
+            {instanceInfo.sub_category}
+          </Typography>
+          <Typography>{instanceInfo.date.split("T")[0]}</Typography>
+          {customPaper(calculateRating())}
+          <Box display="flex" justifyContent="center" marginTop="2.5rem">
+            <Typography>{"Description: "}</Typography>
+          </Box>
+          <Typography className={classes.descriptionAndReasons}>
+            {instanceInfo.description}
+          </Typography>
+          <Box display="flex" justifyContent="center" marginTop="2.5rem">
+            <Typography>{"Reason: "}</Typography>
+          </Box>
+          <Typography className={classes.descriptionAndReasons}>
+            {instanceInfo.reason}
+          </Typography>
+        </Grid>
+
+        <Grid
+          container
+          direction="column"
+          justifyContent="stretch"
+          alignItems="center"
+        >
+          <Grid item>
+            <Button variant="contained" className={classes.back} onClick={Back}>
+              Back
+            </Button>
+          </Grid>
+        </Grid>
+
+        <Paper
+          sx={{
+            p: 2,
+            margin: "auto",
+            maxWidth: "70%",
+            flexGrow: 1,
+            marginTop: "2rem",
+          }}
+        >
+          <Grid
+            container
+            alignItems="center"
+            justifyContent="center"
+            spacing={3}
+          >
+            <Grid item>
+              <AccountCircle />
+            </Grid>
+
+            <Grid item xs={7}>
+              <TextField
+                id="add-comment"
+                label="Add a public comment..."
+                placeholder="Placeholder"
+                multiline
+                variant="standard"
+                fullWidth
+                onChange={changeComment}
+              />
+            </Grid>
+            <Grid item xs={2}>
+              <Box sx={{ minWidth: "3rem", maxWidth: "7rem" }}>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label">rating</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={commentRating}
+                    label="commentRating"
+                    onChange={handleChange}
+                  >
+                    <MenuItem value={1}>1</MenuItem>
+                    <MenuItem value={2}>2</MenuItem>
+                    <MenuItem value={3}>3</MenuItem>
+                    <MenuItem value={4}>4</MenuItem>
+                    <MenuItem value={5}>5</MenuItem>
+                    <MenuItem value={6}>6</MenuItem>
+                    <MenuItem value={7}>7</MenuItem>
+                    <MenuItem value={8}>8</MenuItem>
+                    <MenuItem value={9}>9</MenuItem>
+                    <MenuItem value={10}>10</MenuItem>
+                  </Select>
+                </FormControl>
+              </Box>
+            </Grid>
+            <Grid item justifySelf="flex-end">
+              <Button
+                variant="contained"
+                onClick={() =>
+                  addComment(
+                    comment,
+                    user.email,
+                    instanceInfo.name,
+                    commentRating
+                  )
+                }
+                disabled={
+                  comment.length === 0 ||
+                  commentRating.length === 0 ||
+                  user === null
+                }
+              >
+                Weigh In
+              </Button>
+            </Grid>
+          </Grid>
+        </Paper>
+        {instanceInfo.comments.map((comment) => (
+          <Comment
+            Email={comment.email}
+            Reason={comment.reason}
+            Rating={comment.rating}
+            Date={comment.date}
+          />
+        ))}
+      </Fragment>
     );
   }
   return null;
